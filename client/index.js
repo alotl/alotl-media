edb12ce387df8dce5882b175ae88e58c147cccb7
Michael Teifel <teifel7@gmail.com>
2022-05-07 11:31:46 -0700

    Setup Webpack

---
+++ client/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// import 'bootstrap';
+
+// ReactDOM.render(
+//     <h1>Hello World</h1>,
+//   document.getElementById('root')
+// );
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<h1>Hello, world!</h1>)
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: path.join(__dirname, 'client', 'index.js'),
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      {
+        test: /\.s?css$/i,
+        use: [
+          'style-loader', 
+          'css-loader', 
+          'postcss-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  mode: 'development',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'client', 'index.html'),
+    }),
+    // require('autoprefixer')
+  ],
+  devServer: {
+    // static: {
+    //   directory: 'index.html',
+    // },
+    port: 8080,
+    proxy: {
+      '/api/**': {
+        target: 'http://localhost:3000'
+      }
+    }
+  },
+}
