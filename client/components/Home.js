827bdbf43c7f907e657af5090e85fd9b7077fc90
Michael Teifel <teifel7@gmail.com>
2022-05-09 15:49:30 -0700

    Setup functionality for creating a new media card

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,13 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import CreateMediaCard from './CreateMediaCard'
 import MediaCard from './MediaCard'
 
 export default function Home(props) {
+  const[reviews, setReviews] = useState([]);
+  function handleNewReview(review) {
+    setReviews([...reviews, review]);
+  }
+  const mediaCards = reviews.map(el => {
+    return <MediaCard review={el} />
+  })
+
+  useEffect(() => {
+    fetch('profile')
+      .then(res => res.json())
+      .then(body => setReviews(body))
+  })
+
   return(
     <div id='reviews-container' className='container'>
       <div className='row pt-5'>
-        <CreateMediaCard />
-        <MediaCard />
+        <CreateMediaCard handleNewReview={handleNewReview} />
+        {mediaCards}
       </div>
     </div>
   )
