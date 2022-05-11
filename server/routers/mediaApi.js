4fd2092e54a55e592d045d439c52e7989f3c17d9
yeunhakim93 <yhyyyhhh@gmail.com>
2022-05-10 18:38:48 -0700

    (initial modification)
    Co-authored-by: Yeunha Kim 24959947+yeunhakim93@users.noreply.github.com
    Co-authored-by: Joseph Park 10874783+joeepark@users.noreply.github.com
    Co-authored-by: Giovanni Flores-lovo 83046950+gfloresl@users.noreply.github.com
    Co-authored-by: Andrea Bennett 99983308+AndreaBennett42@users.noreply.github.com
    Co-authored-by: William Robson 96151253+wrobson5467@users.noreply.github.com

--- server/routers/mediaApi.js
+++ server/routers/mediaApi.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const mediaController = require('./mediaController')
+const mediaController = require('../controllers/mediaController')
 
 const router = express.Router();
 
