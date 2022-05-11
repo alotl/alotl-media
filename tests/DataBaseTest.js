b4e11be005ebc96a514a39b28b0e46a54c310ee1
Joseph Park <joeepark@gmail.com>
2022-05-10 18:43:07 -0700

    Signup Initial Merge
    Co-authored-by: Yeunha Kim 24959947+yeunhakim93@users.noreply.github.com
    Co-authored-by: Joseph Park 10874783+joeepark@users.noreply.github.com
    Co-authored-by: Giovanni Flores-lovo 83046950+gfloresl@users.noreply.github.com
    Co-authored-by: Andrea Bennett 99983308+AndreaBennett42@users.noreply.github.com
    Co-authored-by: William Robson 96151253+wrobson5467@users.noreply.github.com

---
+++ tests/DataBaseTest.js
@@ -0,0 +1,10 @@
+const path = require('path');
+const fs = require('fs');
+const testJsonFile = path.resolve(__dirname, './MockDB.js');
+
+describe('signup a user', () => {
+   beforeall((done) => {
+    fs.writeFile(testJsonFile, JSOn.stringify([]), () =>
+    )
+   })
+})
